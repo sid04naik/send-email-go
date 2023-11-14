32dce978525cc74d97cd2aaaf7a50b420a81fea8
sid04naik <siddhantnaik.dev@gmail.com>
2023-11-14 22:01:59 +0530

    added actual email flow

--- cmd/main.go
+++ cmd/main.go
@@ -4,27 +4,28 @@ import (
 	"fmt"
 
 	"github.com/sid04naik/send-email-go/config"
+	"github.com/sid04naik/send-email-go/helper"
 )
 
 var Config *config.Config
 
 func main() {
-	// emailConfig := Config.EmailConfig
-	// toEmailAddress := []string{
-	// 	"gotester@mailinator.com",
-	// }
-	// var message string = "This is test email"
-	// var subject string = "This is email Subject"
-	// fromEmail := emailConfig.AUTH.USER
+	emailConfig := Config.EmailConfig
+	toEmailAddress := []string{
+		"gotester@mailinator.com",
+	}
+	var message string = "This is test email"
+	var subject string = "This is email Subject"
+	fromEmail := emailConfig.AUTH.USER
 
-	// var signal = make(chan struct{})
+	var signal = make(chan struct{})
 
-	// emailHelper := helper.EmailHelper{
-	// 	Config: Config,
-	// }
-	// fmt.Println("Send Email Initiated")
-	// go emailHelper.SendEmail(fromEmail, toEmailAddress, subject, message, signal)
-	// <-signal
+	emailHelper := helper.EmailHelper{
+		Config: Config,
+	}
+	fmt.Println("Send Email Initiated")
+	go emailHelper.SendEmail(fromEmail, toEmailAddress, subject, message, signal)
+	<-signal
 	fmt.Println("Email Send Successfully")
 }
 
