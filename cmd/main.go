5dd5287666f212c35240f5aa39a27bbd2c86d16f
sid04naik <siddhantnaik.dev@gmail.com>
2023-11-14 19:48:00 +0530

    updated loading env file fix

--- cmd/main.go
+++ cmd/main.go
@@ -4,34 +4,32 @@ import (
 	"fmt"
 
 	"github.com/sid04naik/send-email-go/config"
-	"github.com/sid04naik/send-email-go/helper"
 )
 
 var Config *config.Config
 
 func main() {
-	emailConfig := Config.EmailConfig
-	toEmailAddress := []string{
-		"gotester@mailinator.com",
-	}
-	var message string = "This is test email"
-	var subject string = "This is email Subject"
-	fromEmail := emailConfig.AUTH.USER
+	// emailConfig := Config.EmailConfig
+	// toEmailAddress := []string{
+	// 	"gotester@mailinator.com",
+	// }
+	// var message string = "This is test email"
+	// var subject string = "This is email Subject"
+	// fromEmail := emailConfig.AUTH.USER
 
-	var signal = make(chan struct{})
+	// var signal = make(chan struct{})
 
-	emailHelper := helper.EmailHelper{
-		Config: Config,
-	}
-	fmt.Println("Send Email Initiated")
-	go emailHelper.SendEmail(fromEmail, toEmailAddress, subject, message, signal)
-	<-signal
+	// emailHelper := helper.EmailHelper{
+	// 	Config: Config,
+	// }
+	// fmt.Println("Send Email Initiated")
+	// go emailHelper.SendEmail(fromEmail, toEmailAddress, subject, message, signal)
+	// <-signal
 	fmt.Println("Email Send Successfully")
 }
 
 func init() {
-
-	configuration, err := config.Configurations()
+	configuration, err := config.Configurations(".env")
 	Config = &configuration
 	if err != nil {
 		panic("error loading configurations")
